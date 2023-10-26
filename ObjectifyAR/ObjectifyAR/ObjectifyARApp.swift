3badcc17b11a15cd0f30de3ddd6ba9adaaf034b9
Mattia Fasoli <fasoxy@gmail.com>
2023-10-26 12:15:33 +0200

    Create ObjectifyAR

---
+++ ObjectifyAR/ObjectifyAR/ObjectifyARApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectifyARApp.swift
+//  ObjectifyAR
+//
+//  Created by Mattia Fasoli on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ObjectifyARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
