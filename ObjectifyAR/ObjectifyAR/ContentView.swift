1972e97a907f52749156444cf0e905ff954ab584
Mattia Fasoli <fasoxy@gmail.com>
2023-10-29 19:31:17 +0100

    Add the logic to Place and to Interact with an AR Object

--- ObjectifyAR/ObjectifyAR/ContentView.swift
+++ ObjectifyAR/ObjectifyAR/ContentView.swift
@@ -7,10 +7,17 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+                .edgesIgnoringSafeArea(.all)
+        }
+        
     }
 }
 
@@ -19,24 +26,32 @@ struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
-
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
-
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
-
-        // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
-
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+        
+        arView.session.run(config)
+        
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) { 
+        
+        let modelEntity = try! ModelEntity.loadModel(named: "jar")
+        let anchorEntity = AnchorEntity(plane: .any)
+        anchorEntity.addChild(modelEntity)
+        uiView.scene.addAnchor(anchorEntity)
+        
+        modelEntity.generateCollisionShapes(recursive: true)
+        
+        uiView.installGestures([.translation, .rotation, .scale], for: modelEntity)
+        
+    }
     
 }
 
