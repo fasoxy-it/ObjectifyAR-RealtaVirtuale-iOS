fbc0c47c1811fd39d96a316e150df57e7824c373
Mattia Fasoli <fasoxy@gmail.com>
2023-10-31 14:36:09 +0100

    Create ModelPickerView and Modify ARViewContainer

--- ObjectifyAR/ObjectifyAR/ContentView.swift
+++ ObjectifyAR/ObjectifyAR/ContentView.swift
@@ -11,17 +11,41 @@ import ARKit
 
 struct ContentView : View {
     
+    @State private var selectedModel: String?
+    
+    private var models: [String] = {
+        
+        let filemanager = FileManager.default
+        
+        guard let path = Bundle.main.resourcePath, let files = try? filemanager.contentsOfDirectory(atPath: path) else {return []}
+    
+        var availableModels: [String] = []
+        
+        for filename in files where filename.hasSuffix("usdz") {
+            let  modelName = filename.replacingOccurrences(of: ".usdz", with: "")
+            availableModels.append(modelName)
+        }
+        
+        return availableModels
+        
+    }()
+    
     var body: some View {
         
         ZStack(alignment: .bottom) {
-            ARViewContainer()
+            ARViewContainer(selectedModel: $selectedModel)
                 .edgesIgnoringSafeArea(.all)
+            
+            ModelPickerView(selectedModel: $selectedModel, models: models)
+            
         }
         
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    
+    @Binding var selectedModel: String?
     
     func makeUIView(context: Context) -> ARView {
         
@@ -40,16 +64,51 @@ struct ARViewContainer: UIViewRepresentable {
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) { 
+    func updateUIView(_ uiView: ARView, context: Context) {
         
-        let modelEntity = try! ModelEntity.loadModel(named: "jar")
-        let anchorEntity = AnchorEntity(plane: .any)
-        anchorEntity.addChild(modelEntity)
-        uiView.scene.addAnchor(anchorEntity)
+        if let modelName = selectedModel {
+            let modelEntity = try! ModelEntity.loadModel(named: modelName)
+            let anchorEntity = AnchorEntity(plane: .any)
+            anchorEntity.addChild(modelEntity)
+            uiView.scene.addAnchor(anchorEntity)
+            
+            modelEntity.generateCollisionShapes(recursive: true)
+            
+            uiView.installGestures([.translation, .rotation, .scale], for: modelEntity)
+        }
         
-        modelEntity.generateCollisionShapes(recursive: true)
+    }
+    
+}
+
+struct ModelPickerView: View {
+    
+    @Binding var selectedModel: String?
+    
+    var models: [String]
+    
+    var body: some View {
         
-        uiView.installGestures([.translation, .rotation, .scale], for: modelEntity)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(0 ..< models.count) { index in
+                    Button(action: {
+                        print("DEBUG: Selected model with name: \(models[index])")
+                        selectedModel = models[index]
+                    }) {
+                        Image(uiImage: UIImage(named: models[index])!)
+                            .resizable()
+                            .frame(height: 80)
+                            .aspectRatio(1/1, contentMode: .fit)
+                            .background(Color.white)
+                            .cornerRadius(12)
+                    }
+                        .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+        .padding(20)
+        .background(Color.black.opacity(0.5))
         
     }
     
